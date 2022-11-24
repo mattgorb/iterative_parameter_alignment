6a2d9ee884cddfd8f8937a5dea69e0d3a5c17574
matthewgorbett <Idontknow8112@@@>
2022-11-24 13:51:52 -0700

    first!

--- mnist_example.py
+++ mnist_example.py
@@ -160,7 +160,7 @@ class Trainer:
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--epochs', type=int, default=50, metavar='N',
                         help='number of epochs to train (default: 14)')
