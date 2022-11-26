8fa3b95077f7759451bff353aaa1a1cb13212af0
matthewgorbett <Idontknow8112@@@>
2022-11-26 08:03:26 -0700

    first!

--- mnist_example2.py
+++ mnist_example2.py
@@ -323,6 +323,7 @@ def generate_mlc(model1, model2, model_new):
 
             if n1=='conv1':
                 print('here0')
+                print(m1_mask)
                 print(mlc[0])
                 print(mlc_mask[0])
 
