5e288cd12c189a9b293d080829cb2715530abffc
matthewgorbett <Idontknow8112@@@>
2022-11-27 20:42:18 -0700

    first!

--- mnist_merge2.py
+++ mnist_merge2.py
@@ -166,7 +166,7 @@ class Trainer:
             data, target = data.to(self.device), target.to(self.device)
             self.optimizer.zero_grad()
             output, sd = self.model(data)
-            loss = self.criterion(output, target)+500*sd
+            loss = self.criterion(output, target)+250*sd
             train_loss+=loss
             loss.backward()
             self.optimizer.step()
