d6311e4baf69db4039cfbb1328f9c00f6a583909
matthewgorbett <Idontknow8112@@@>
2022-11-30 20:28:35 -0700

    first!

--- mnist_model_merge.py
+++ mnist_model_merge.py
@@ -40,7 +40,7 @@ class LinearMerge(nn.Linear):
         weights_diff=torch.tensor(0)
         if self.weight_align is not None:
             #using absolute error here.  Need to test using MSE loss
-            weights_diff=torch.sum((self.weight-self.weight_align).abs())
+            weights_diff=torch.mean((self.weight-self.weight_align).abs())
             #weights_diff=torch.mean((self.weight-self.weight_align)**2)
         return x, weights_diff
 
@@ -129,7 +129,7 @@ class Trainer:
         self.model = model
         self.train_loader, self.test_loader=datasets[0],datasets[1]
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
-        self.criterion=nn.CrossEntropyLoss(reduction='sum')
+        self.criterion=nn.CrossEntropyLoss(reduction='mean')
         #self.scheduler = CosineAnnealingLR(self.optimizer, T_max=epochs)
         self.device=device
         self.save_path=save_path
