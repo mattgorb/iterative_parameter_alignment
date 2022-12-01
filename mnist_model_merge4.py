619cb8ea6f8c801f295544046ecef9597e15b658
matthewgorbett <Idontknow8112@@@>
2022-11-30 21:58:39 -0700

    first!

--- mnist_model_merge4.py
+++ mnist_model_merge4.py
@@ -75,47 +75,25 @@ class Net(nn.Module):
 
 
 def get_datasets(args):
-    #not using
     transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
 
     if args.baseline:
-        dataset1 = datasets.MNIST(f'{args.base_dir}data', train=True, download=True, transform=None)
-        test_dataset = datasets.MNIST(f'{args.base_dir}data', train=False, transform=None)
+        dataset1 = datasets.MNIST(f'{args.base_dir}data', train=True, download=True, transform=transform)
+        test_dataset = datasets.MNIST(f'{args.base_dir}data', train=False, transform=transform)
         train_loader = DataLoader(dataset1, batch_size=args.batch_size, shuffle=True)
         test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
         return train_loader, test_loader
     else:
-        dataset1 = datasets.MNIST(f'{args.base_dir}data', train=True, download=True, transform=None)
-        dataset2 = datasets.MNIST(f'{args.base_dir}data', train=True, transform=None)
+        dataset1 = datasets.MNIST(f'{args.base_dir}data', train=True, download=True, transform=transform)
 
-        #split dataset in half by labels
-        labels=torch.unique(dataset1.targets)
-        ds1_labels=labels[:len(labels)//2]
-        ds2_labels=labels[len(labels)//2:]
-        print(f'ds1_labels: {ds1_labels}')
-        print(f'ds2_labels: {ds2_labels}')
-        ds1_indices = [idx for idx, target in enumerate(dataset1.targets) if target in ds1_labels]
-        ds2_indices = [idx for idx, target in enumerate(dataset1.targets) if target in ds2_labels]
 
-        '''
-        #use this code for p/1-p split.  need to test
-        #p=0.8
-        ds1_indices=ds1_indices[:int(len(ds1_indices)*p)]+ds2_indices[int(len(ds2_indices)*p):]
-        ds2_indices=ds1_indices[int(len(ds1_indices)*p):]+ds2_indices[:int(len(ds2_indices)*p)]
-        '''
-
-        '''
-        #use this code to split dataset down middle. need to test
-        dataset1.data, dataset1.targets = dataset1.data[:int(len(dataset1.targets)/2)], dataset1.targets[:int(len(dataset1.targets)/2)]
+        dataset1.data, dataset1.targets = dataset1.data[:15000], dataset1.targets[:15000]
         dataset2.data, dataset2.targets = dataset2.data[int(len(dataset1.targets)/2):], dataset2.targets[int(len(dataset1.targets)/2):]
-        '''
 
-        dataset1.data, dataset1.targets = dataset1.data[ds1_indices], dataset1.targets[ds1_indices]
-        dataset2.data, dataset2.targets = dataset2.data[ds2_indices], dataset2.targets[ds2_indices]
-        assert(set(ds1_indices).isdisjoint(ds2_indices))
+
 
 
         test_dataset = datasets.MNIST(f'{args.base_dir}data', train=False,  transform=transform)
