de56e6146741cd97a794fc0bdd14e887435b5c38
matthewgorbett <Idontknow8112@@@>
2022-12-01 19:30:44 -0700

    first!

--- mnist_model_merge.py
+++ mnist_model_merge.py
@@ -240,8 +240,8 @@ class Merge_Iterator:
 
 
             print(f'Merge Iteration: {iter} \n'
-                      f'\tModel 1 Train loss: {model1_trainer.train_loss}, Test loss: {model1_trainer.test_acc},  Test accuracy: {model1_trainer.test_acc}\n'
-                      f'\tModel 2 Train loss: {model2_trainer.train_loss}, Test loss: {model2_trainer.test_acc},  Test accuracy: {model2_trainer.test_acc}')
+                      f'\tModel 1 Train loss: {model1_trainer.train_loss}, Test loss: {model1_trainer.test_loss},  Test accuracy: {model1_trainer.test_acc}\n'
+                      f'\tModel 2 Train loss: {model2_trainer.train_loss}, Test loss: {model2_trainer.test_loss},  Test accuracy: {model2_trainer.test_acc}')
 
 def main():
     # Training settings
