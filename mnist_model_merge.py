709c6a00b3e09809f5c88f0770e1a01fe110e260
matthewgorbett <Idontknow8112@@@>
2022-12-01 19:34:13 -0700

    first!

--- mnist_model_merge.py
+++ mnist_model_merge.py
@@ -130,7 +130,7 @@ class Trainer:
         self.args = args
         self.model = model
         self.train_loader, self.test_loader=datasets[0],datasets[1]
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
         self.criterion=nn.CrossEntropyLoss(reduction='sum')
         self.device=device
         self.save_path=save_path
