c9103d589a37cd93236b4c4921f2cf9419c40433
matthewgorbett <Idontknow8112@@@>
2022-12-10 08:42:38 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -290,7 +290,7 @@ class Merge_Iterator:
 
             #print(model1.fc2.weight[0][:10])
 
-            model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
+            #model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
             model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
@@ -300,8 +300,8 @@ class Merge_Iterator:
 
             set_weight_align_param(model1, model2,self.args)
 
-            #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
-            #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
+            model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
+            model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
             print(model1.fc2.weight[0][:10])
 
             print(model2.fc2.weight_align[0][:10])
