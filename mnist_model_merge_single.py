2acc402acd355082009e5fd83639bf62271e0f69
matthewgorbett <Idontknow8112@@@>
2022-12-10 21:35:09 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -292,7 +292,7 @@ class Merge_Iterator:
             #print(model1.fc2.weight[0][:10])
 
             #model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
-            #model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
+            model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
