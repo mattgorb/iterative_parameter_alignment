bb1e9041f33e152d38a3b9c42ee2b6d533131319
matthewgorbett <Idontknow8112@@@>
2022-12-11 06:05:02 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -255,8 +255,8 @@ def set_weight_align_param(model1, model2,args):
             #We only want to merge one models weights in this file
             #m1.weight_align=nn.Parameter(m2.weight, requires_grad=True)
             #if args.detach():
-            #m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
-            m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
+            m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
+            #m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
 
 class Merge_Iterator:
     def __init__(self, args,datasets, device,weight_dir):
@@ -295,20 +295,22 @@ class Merge_Iterator:
             model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
-
-
+            print("HERE")
+            print(model1.fc2.weight[0][:10])
             model1_trainer.fit()
             print(model1.fc2.weight[0][:10])
             model2_trainer.fit()
-            print(model1.fc2.weight[0][:10])
+
             #print(model2_trainer.test_acc)
             set_weight_align_param(model1, model2, self.args)
+
+            print(model1.fc2.weight[0][:10])
             #model2_trainer.fit()
             #print(model2_trainer.test_acc)
             #sys.exit()
             #set_weight_align_param(model1, model2, self.args)
-            #model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
-            #model1.fc2.weight = nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
+            model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
+            model1.fc2.weight = nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
 
             #print(model1.fc2.weight[0][:10])
             #print(model2.fc2.weight_align[0][:10])
