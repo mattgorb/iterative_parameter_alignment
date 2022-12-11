9d4990d98da1403766b3ecc0b4fccbdc3c1a2482
matthewgorbett <Idontknow8112@@@>
2022-12-10 20:50:59 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -255,8 +255,8 @@ def set_weight_align_param(model1, model2,args):
             #We only want to merge one models weights in this file
             #m1.weight_align=nn.Parameter(m2.weight, requires_grad=True)
             #if args.detach():
-            #m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
-            m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
+            m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
+            #m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
 
 class Merge_Iterator:
     def __init__(self, args,datasets, device,weight_dir):
@@ -286,7 +286,7 @@ class Merge_Iterator:
         model2_trainer = Trainer(self.args, [self.train_loader2, self.test_dataset], model2, self.device, f'{self.weight_dir}model2_0.pt','model2_double')
 
 
-        set_weight_align_param(model1, model2, self.args)
+
         for iter in range(merge_iterations):
 
             #print(model1.fc2.weight[0][:10])
@@ -299,7 +299,7 @@ class Merge_Iterator:
             model1_trainer.fit()
             model2_trainer.fit()
 
-
+            set_weight_align_param(model1, model2, self.args)
 
             #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
             #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
