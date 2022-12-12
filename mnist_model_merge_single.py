467f60217f51028a7a1736bb83f6736a1cb75ea6
matthewgorbett <Idontknow8112@@@>
2022-12-12 07:53:03 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -273,8 +273,10 @@ class Merge_Iterator:
             model1_trainer.fit()
 
             if iter>0:
-                model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
-                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+                #model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
+                #model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+                model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
+                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
                 #model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
                 model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
             if iter>0:
