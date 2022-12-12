9b6599d4eacd374bcad8f79effd351f65b6e31be
matthewgorbett <Idontknow8112@@@>
2022-12-11 20:06:27 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -265,19 +265,17 @@ class Merge_Iterator:
             # model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
             if iter>0:
-                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.to(self.device), requires_grad=True)
-                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.to(self.device), requires_grad=True)#.clone().detach()
+                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
+                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)
             model1_trainer.fit()
 
-            #if iter>0:
-            model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
-            model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
-
+            if iter>0:
+                set_weight_align_param(model1, model2, self.args)
             model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
             model2_trainer.fit()
 
-            #set_weight_align_param(model1, model2, self.args)
+            set_weight_align_param(model1, model2, self.args)
 
 
             # model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
