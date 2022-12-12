42e1385e6c4b82067273b8a7d0b9dd611118842c
matthewgorbett <Idontknow8112@@@>
2022-12-12 09:31:11 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -271,11 +271,10 @@ class Merge_Iterator:
             #model1.train()
             model1_trainer.fit()
 
-            if iter>0:
+            '''if iter>0:
                 model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
-                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
-                #model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
-                #model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
+                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)'''
+
 
                 
             print(model1.fc2.weight[0][:5])
@@ -289,8 +288,8 @@ class Merge_Iterator:
             if iter>0:
                 #print(model1_trainer.optimizer.param_groups)
                 print(model1_trainer.optimizer.state_dict())
-                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
-                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
+                #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
+                #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
                 #print(model1_trainer.optimizer.param_groups)
                 print(model1_trainer.optimizer.state_dict())
                 sys.exit()
@@ -301,8 +300,8 @@ class Merge_Iterator:
 
 
 
-            #if iter==0:
-                #set_weight_align_param(model1, model2, self.args)
+            if iter==0:
+                set_weight_align_param(model1, model2, self.args)
 
 
             # model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
