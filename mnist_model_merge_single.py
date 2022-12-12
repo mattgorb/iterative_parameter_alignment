e1f749332d4e55af5c2f8e71913eba53bd250448
matthewgorbett <Idontknow8112@@@>
2022-12-12 07:55:24 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -295,9 +295,10 @@ class Merge_Iterator:
                 model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
                 model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
             else:
-                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
-                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
-
+                #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
+                #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
+                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.to(self.device), requires_grad=True)
+                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.to(self.device), requires_grad=True)#.clone().detach()
 
             if iter>0:
                 print(model1.fc2.weight[0][:5])
