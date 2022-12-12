b86e1ed95aa5e170d68cf211e9137c58977cfff7
matthewgorbett <Idontknow8112@@@>
2022-12-11 19:27:02 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -228,8 +228,8 @@ def set_weight_align_param(model1, model2, args):
             # We only want to merge one models weights in this file
             # m1.weight_align=nn.Parameter(m2.weight, requires_grad=True)
             # if args.detach():
-            #m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
-            m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
+            m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
+            #m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
 
 
 class Merge_Iterator:
@@ -264,10 +264,13 @@ class Merge_Iterator:
             # model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             # model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
+            if iter>0:
+                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
+                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)
             model1_trainer.fit()
 
-            #if iter>0:
-                #set_weight_align_param(model1, model2, self.args)
+            if iter>0:
+                set_weight_align_param(model1, model2, self.args)
             model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
             model2_trainer.fit()
@@ -278,8 +281,7 @@ class Merge_Iterator:
             # model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
             # model1.fc2.weight = nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
 
-            # model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
-            # model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)
+
 
             print(f'Merge Iteration: {iter} \n'
                   f'\tModel 1 Train loss: {model1_trainer.train_loss}, Test loss: {model1_trainer.test_loss},  Test accuracy: {model1_trainer.test_acc}\n'
