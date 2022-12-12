d4a790f7cd7efad02be2de12b3f6ee68f47cf216
matthewgorbett <Idontknow8112@@@>
2022-12-12 11:16:26 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -271,9 +271,9 @@ class Merge_Iterator:
             model1_trainer.fit()
 
 
-            '''if iter>0:
+            if iter>0:
                 model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
-                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)'''
+                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
 
             if iter>0:
                 print(model1.fc2.weight[0][:5])
@@ -292,8 +292,8 @@ class Merge_Iterator:
                 model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
                 model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
                 model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)'''
-            if iter==0:
-                set_weight_align_param(model1, model2, self.args)
+            #if iter==0:
+                #set_weight_align_param(model1, model2, self.args)
             if iter>0:
                 print(model1.fc2.weight[0][:5])
                 print(model2.fc2.weight_align[0][:5])
