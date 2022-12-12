3e3249d3483971bbe18ef96dce2a63a6d6068178
matthewgorbett <Idontknow8112@@@>
2022-12-12 08:00:08 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -272,32 +272,32 @@ class Merge_Iterator:
 
             model1_trainer.fit()
 
-            '''if iter>0:
-                #model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
-                #model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
-                model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
-                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
+            if iter>0:
+                model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
+                model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+                #model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
+                #model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
                 #model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
                 
             if iter>0:
                 print(model1.fc2.weight[0][:5])
-                print(model2.fc2.weight_align[0][:5])'''
+                print(model2.fc2.weight_align[0][:5])
 
             model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
             model2_trainer.fit()
 
 
-            '''if iter==0:
-                #model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
-                #model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
-                model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
-                model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
+            if iter==0:
+                model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
+                model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+                #model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.to(self.device), requires_grad=True)
+                #model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.to(self.device), requires_grad=True)
             else:
-                #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
-                #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
-                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.to(self.device), requires_grad=True)
-                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.to(self.device), requires_grad=True)#.clone().detach()'''
+                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
+                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)#.clone().detach()
+                #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.to(self.device), requires_grad=True)
+                #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.to(self.device), requires_grad=True)#.clone().detach()
 
             if iter>0:
                 print(model1.fc2.weight[0][:5])
@@ -305,7 +305,7 @@ class Merge_Iterator:
 
 
             #if iter==0:
-            set_weight_align_param(model1, model2, self.args)
+                #set_weight_align_param(model1, model2, self.args)
 
 
             # model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
