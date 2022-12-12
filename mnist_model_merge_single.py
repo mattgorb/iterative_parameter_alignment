2022af504b33f2ffaea454c26e3b8dc245feee27
matthewgorbett <Idontknow8112@@@>
2022-12-11 19:51:41 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -265,8 +265,8 @@ class Merge_Iterator:
             # model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
             if iter>0:
-                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach(), requires_grad=True)
-                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach(), requires_grad=True)
+                model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
+                model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)#.clone().detach()
             model1_trainer.fit()
 
             #if iter>0:
