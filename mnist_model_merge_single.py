e2cc2d2e9195d9ad49d0af8506fa367fd1c59477
matthewgorbett <Idontknow8112@@@>
2022-12-11 17:55:31 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -321,9 +321,7 @@ class Merge_Iterator:
             if iter>0:
                 print(model2.fc2.weight_align[0][:10])
             model2_trainer.fit()
-            if iter>0:
-                print(model2.fc2.weight_align[0][:10])
-                sys.exit()
+
             #print(model2_trainer.test_acc)
             set_weight_align_param(model1, model2, self.args)
             print("HERE")
@@ -338,14 +336,16 @@ class Merge_Iterator:
 
             #print(model1.fc2.weight[0][:10])
             #print(model2.fc2.weight_align[0][:10])
-            model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
-            model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)
+            #model1.fc1.weight=nn.Parameter(model2.fc1.weight_align, requires_grad=True)
+            #model1.fc2.weight=nn.Parameter(model2.fc2.weight_align, requires_grad=True)
             #print(model1.fc2.weight[0][:10])
 
             print(f'Merge Iteration: {iter} \n'
                   f'\tModel 1 Train loss: {model1_trainer.train_loss}, Test loss: {model1_trainer.test_loss},  Test accuracy: {model1_trainer.test_acc}\n'
                   f'\tModel 2 Train loss: {model2_trainer.train_loss}, Test loss: {model2_trainer.test_loss},  Test accuracy: {model2_trainer.test_acc}')
-
+            if iter>0:
+                print(model2.fc2.weight_align[0][:10])
+                sys.exit()
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
