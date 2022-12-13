e08a6a62f625034af65bfa2a1fe2dd332fc0d179
matthewgorbett <Idontknow8112@@@>
2022-12-12 20:58:27 -0700

    merge_single

--- mnist_model_merge_single.py
+++ mnist_model_merge_single.py
@@ -180,7 +180,17 @@ class Trainer:
                         if batch_idx % 25 == 0:
                             self.wa1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
                             self.wa2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
+        if self.args.graphs:
+            if self.model.fc1.weight is not None:
+                if batch_idx%25==0:
+                    self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
+                    self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
 
+            if hasattr(self.model.fc1, 'weight_align'):
+                if self.model.fc1.weight_align is not None:
+                    if batch_idx % 25 == 0:
+                        self.wa1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
+                        self.wa2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
 
         train_loss /= len(self.train_loader.dataset)
         return train_loss
