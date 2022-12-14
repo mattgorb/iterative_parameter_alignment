d4413f389a480127340c89993e85ac24761e46b4
matthewgorbett <Idontknow8112@@@>
2022-12-14 11:26:52 -0700

    merge_single

--- mnist_double.py
+++ mnist_double.py
@@ -290,18 +290,18 @@ class Merge_Iterator:
         for iter in range(merge_iterations):
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
-            if iter%2==0:
-                print("HERE")
-                model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
-                model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
 
-            model1_trainer.fit()
-            model2_trainer.fit()
+            model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
+            model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
 
 
+            for iter2 in range(2):
+                model1_trainer.fit()
+                model2_trainer.fit()
 
-            if iter==1:
-                print("HERE2")
+
+
+            if iter==0:
                 set_weight_align_param(model1, model2, self.args)
 
             if iter>0:
