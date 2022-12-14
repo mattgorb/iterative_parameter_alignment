1aa362cf81ea35a08b105d480024f68dc1eb92b9
matthewgorbett <Idontknow8112@@@>
2022-12-14 08:20:53 -0700

    merge_single

--- mnist_double.py
+++ mnist_double.py
@@ -290,7 +290,7 @@ class Merge_Iterator:
         for iter in range(merge_iterations):
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
-            if iter%2==0:
+            if iter%4==0:
                 print("HERE")
                 model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
                 model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
