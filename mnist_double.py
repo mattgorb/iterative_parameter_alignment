bc1c2fad69b2e12317bedda23784b71ca01f227f
matthewgorbett <Idontknow8112@@@>
2022-12-16 08:10:15 -0700

    merge_single

--- mnist_double.py
+++ mnist_double.py
@@ -341,7 +341,7 @@ class Merge_Iterator:
                              })
             print(df)
             print(model1_trainer.train_iter_list)
-            sys.exit()
+            #sys.exit()
             df.to_csv('norms/norms_double.csv')
 
             df=pd.DataFrame({'weight_diff_layer1':wd1,
