622711c2ce037fdc69fcbc3f3eb22030c0438e5d
matthewgorbett <Idontknow8112@@@>
2022-12-16 06:54:27 -0700

    merge_single

--- mnist_double.py
+++ mnist_double.py
@@ -130,13 +130,17 @@ class Trainer:
         self.device = device
         self.save_path = save_path
         self.model_name = model_name
+
         self.fc1_norm_list = []
         self.fc2_norm_list = []
         self.wa1_norm_list = []
         self.wa2_norm_list = []
+        self.train_iter_list=[]
+        self.train_iter=0
 
     def fit(self, log_output=False):
         self.train_loss = 1e6
+        self.train_iter+=1
         for epoch in range(1, self.args.epochs + 1):
             epoch_loss = self.train()
             self.train_loss = epoch_loss
@@ -161,6 +165,7 @@ class Trainer:
             if self.model.fc1.weight is not None:
                 self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
                 self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
+                self.train_iter_list.append(self.train_iter)
 
             if hasattr(self.model.fc1, 'weight_align'):
                 if self.model.fc1.weight_align is not None:
@@ -191,7 +196,7 @@ class Trainer:
                     if batch_idx in [10,25,50,75]:
                         self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
                         self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
-
+                        self.train_iter_list.append(self.train_iter)
                 if hasattr(self.model.fc1, 'weight_align'):
 
                     if self.model.fc1.weight_align is not None:
@@ -205,9 +210,9 @@ class Trainer:
 
         if self.args.graphs:
             if self.model.fc1.weight is not None:
-                    self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
-                    self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
-
+                self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
+                self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
+                self.train_iter_list.append(self.train_iter)
             if hasattr(self.model.fc1, 'weight_align'):
                 if self.model.fc1.weight_align is not None:
                         self.wa1_norm_list.append(torch.norm(self.model.fc1.weight_align, p=1).detach().cpu().item())
@@ -250,9 +255,7 @@ def set_weight_align_param(model1, model2, args):
             Alternatively we could set m1.weight=m2.weight_align after merge model is done training.  
             '''
             # We only want to merge one models weights in this file
-            # m1.weight_align=nn.Parameter(m2.weight, requires_grad=True)
-            # if args.detach():
-            #m2.weight_align = nn.Parameter(m1.weight.clone().detach(), requires_grad=True)
+            # m1.weight_align=nn.Parameter(m2.weight, requires_grad=True)s
             m2.weight_align = nn.Parameter(m1.weight, requires_grad=True)
             m1.weight_align = nn.Parameter(m2.weight, requires_grad=True)
 
@@ -277,10 +280,11 @@ class Merge_Iterator:
 
     def run(self):
         merge_iterations = self.args.merge_iter
-        intra_merge_iterations=[25 for i in range(2)]+[15 for i in range(2)]+[10 for i in range(2)]+[5 for i in range(4)]+[2 for i in range(10)]+[1 for i in range(1000)]
+        intra_merge_iterations=[15 for i in range(2)]+[10 for i in range(2)]+[5 for i in range(4)]+[2 for i in range(10)]+[1 for i in range(1000)]
 
         model1 = Net(self.args, weight_merge=True).to(self.device)
         model2 = Net(self.args, weight_merge=True).to(self.device)
+
         model1_trainer = Trainer(self.args, [self.train_loader1, self.test_dataset], model1, self.device,
                                  f'{self.weight_dir}model1_0.pt', 'model1_double')
         model2_trainer = Trainer(self.args, [self.train_loader2, self.test_dataset], model2, self.device,
@@ -288,28 +292,28 @@ class Merge_Iterator:
 
         wd1=[]
         wd2=[]
+        mi=[]
 
+        '''
+        AdaDelta works with re-initialization (because of the adadptive state)
+        SGD works with one initialization, but requires tuning the weight_align_factor and learning rate.
         model1_trainer.optimizer = optim.SGD(model1.parameters(), lr=self.args.lr)
         model2_trainer.optimizer = optim.SGD(model2.parameters(), lr=self.args.lr)
+        '''
+
         for iter in range(merge_iterations):
-            #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
-            #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
-            #model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
-            #model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
-            #model1_trainer.optimizer = optim.Adadelta(model1.parameters(), )
-            #model2_trainer.optimizer = optim.Adadelta(model2.parameters(), )
+            model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
+            model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
 
-
-            #print(f'Inter Merge Iterations: {intra_merge_iterations[iter]}')
-            #for iter2 in range(intra_merge_iterations[iter]):
-            for iter2 in range(1):
+            print(f'Inter Merge Iterations: {intra_merge_iterations[iter]}')
+            for iter2 in range(intra_merge_iterations[iter]):
                 model1_trainer.fit()
                 model2_trainer.fit()
                 if iter>0:
                     wd1.append(torch.sum((model1_trainer.model.fc1.weight-model2_trainer.model.fc1.weight).abs()).detach().cpu().item())
                     wd2.append(torch.sum((model1_trainer.model.fc2.weight-model2_trainer.model.fc2.weight).abs()).detach().cpu().item())
-
+                    mi.append(iter)
 
             if iter==0:
                 set_weight_align_param(model1, model2, self.args)
@@ -328,12 +332,16 @@ class Merge_Iterator:
                              'model2_wa1':model2_trainer.wa1_norm_list,
                              'model2_wa2':model2_trainer.wa2_norm_list,
                              'model1_wa1': model1_trainer.wa1_norm_list,
-                             'model1_wa2': model1_trainer.wa2_norm_list
+                             'model1_wa2': model1_trainer.wa2_norm_list,
+                             'train_epoch':model1_trainer.train_iter_list
                              })
             df.to_csv('norms/norms_double.csv')
 
             df=pd.DataFrame({'weight_diff_layer1':wd1,
-                             'weight_diff_layer2':wd2})
+                             'weight_diff_layer2':wd2,
+                             'epoch':model1_trainer.train_iter_list,
+                             'merge_iter':mi
+                             })
             df.to_csv('norms/weight_diff_double.csv')
 
 def main():
