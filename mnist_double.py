8cc18dcd4fde5c4ee5968724dcdc704fa1d63568
matthewgorbett <Idontknow8112@@@>
2022-12-15 19:26:45 -0700

    merge_single

--- mnist_double.py
+++ mnist_double.py
@@ -289,14 +289,15 @@ class Merge_Iterator:
         wd1=[]
         wd2=[]
 
+        model1_trainer.optimizer = optim.SGD(model1.parameters(), lr=self.args.lr)
+        model2_trainer.optimizer = optim.SGD(model2.parameters(), lr=self.args.lr)
         for iter in range(merge_iterations):
             #model1_trainer=self.train_single(model1, f'{self.weight_dir}model1_{iter}.pt', self.train_loader1,'model1_single')
             #model2_trainer = self.train_single(model2, f'{self.weight_dir}model2_{iter}.pt', self.train_loader2, 'model2_single')
 
             #model1_trainer.optimizer=optim.Adam(model1.parameters(), lr=self.args.lr)
             #model2_trainer.optimizer=optim.Adam(model2.parameters(), lr=self.args.lr)
-            #model1_trainer.optimizer=optim.SGD(model1.parameters(), lr=self.args.lr)
-            #model2_trainer.optimizer=optim.SGD(model2.parameters(), lr=self.args.lr)
+
 
             print(f'Inter Merge Iterations: {intra_merge_iterations[iter]}')
             #for iter2 in range(intra_merge_iterations[iter]):
