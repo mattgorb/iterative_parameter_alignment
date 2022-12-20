485cb28853ba3f1de82fd717587066c21a5c6e4b
matthewgorbett <Idontknow8112@@@>
2022-12-20 12:14:07 -0700

    merge_single

--- mnist_double_permute.py
+++ mnist_double_permute.py
@@ -11,6 +11,7 @@ import random
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from matplotlib import pyplot as plt
 import pandas as pd
+import torch.autograd as autograd
 
 def set_seed(seed):
     random.seed(seed)
@@ -25,13 +26,34 @@ def linear_init(in_dim, out_dim, bias=False, args=None, ):
     layer.init(args)
     return layer
 
+class GetSubnet(autograd.Function):
+    @staticmethod
+    def forward(ctx, scores, k):
+        # Get the supermask by sorting the scores and using the top k%
+        out = scores.clone()
+        _, idx = scores.flatten().sort()
+        j = int((1 - k) * scores.numel())
 
+        # flat_out and out access the same memory.
+        flat_out = out.flatten()
+        flat_out[idx[:j]] = 0
+        flat_out[idx[j:]] = 1
+
+        return out
+
+    @staticmethod
+    def backward(ctx, g):
+        # send the gradient g straight-through on the backward pass.
+        return g, None
 
 
 class LinearMerge(nn.Linear):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.weight_align = None
+        print(self.weight.size())
+        sys.exit()
+        self.scores = nn.Parameter(torch.Tensor(self.weight.size()))
 
     def init(self, args):
         self.args = args
