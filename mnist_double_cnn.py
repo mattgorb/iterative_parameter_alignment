ea6a54138765bb2777814cef6a9f17f1064afd1e
matthewgorbett <Idontknow8112@@@>
2022-12-20 11:31:39 -0700

    merge_single

--- mnist_double_cnn.py
+++ mnist_double_cnn.py
@@ -38,6 +38,8 @@ class ConvMerge(nn.Conv2d):
         # self.args.weight_seed+=1
 
     def forward(self, x):
+        print(self.weight)
+        sys.exit()
         x = F.conv2d(
             x, self.weight, self.bias, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=self.groups
         )
