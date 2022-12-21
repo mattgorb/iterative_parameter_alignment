35f76171046839ffed0e31b62795e727120a6f23
matthewgorbett <Idontknow8112@@@>
2022-12-21 12:08:01 -0700

    merge_single

--- cifar10_double.py
+++ cifar10_double.py
@@ -88,42 +88,73 @@ class Conv4(nn.Module):
         self.bias=True
         self.args=args
         self.weight_merge=weight_merge
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=self.bias)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=self.bias)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=self.bias)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=self.bias)
 
+        if self.weight_merge:
+            self.conv1 = conv_init(3, 64, args=self.args)
+            self.conv2 = conv_init(64, 64, args=self.args)
+            self.conv3 = conv_init(64, 128, args=self.args)
+            self.conv4 = conv_init(128, 128, args=self.args)
+            self.fc1=linear_init(32*32*8, 256, args=self.args)
+            self.fc2=linear_init(256, 256, args=self.args)
+            self.fc3=linear_init(256, 10, args=self.args)
+        else:
+
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=self.bias)
+            self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=self.bias)
+            self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1, bias=self.bias)
+            self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1, bias=self.bias)
+            self.fc1=nn.Linear(32*32*8, 256, bias=self.bias)
+            self.fc2=nn.Linear(256, 256, bias=self.bias)
+            self.fc3=nn.Linear(256, 10, bias=self.bias)
 
         self.max_pool=nn.MaxPool2d((2, 2))
         self.relu=nn.ReLU(True)
         #self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
 
-        self.fc1=nn.Linear(32*32*8, 256, bias=self.bias)
-        self.fc2=nn.Linear(256, 256, bias=self.bias)
-        self.fc3=nn.Linear(256, 10, bias=self.bias)
+
 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
-        x=self.conv1(x)
-        x=self.relu(x)
-        x=self.conv2(x)
-        x=self.relu(x)
-        x=self.max_pool(x)
-        x=self.conv3(x)
-        x=self.relu(x)
-        x=self.conv4(x)
-        x=self.relu(x)
+        if self.weight_merge:
+            x,wd1 = self.conv1(x)
+            x = self.relu(x)
+            x,wd2 = self.conv2(x)
+            x = self.relu(x)
+            x = self.max_pool(x)
+            x,wd3 = self.conv3(x)
+            x = self.relu(x)
+            x,wd4 = self.conv4(x)
+            x = self.relu(x)
+            x = self.max_pool(x)
+            x = x.view(x.size(0), 8192)
+            x,wd5 = self.fc1(x)
+            x = self.relu(x)
+            x,wd6 = self.fc2(x)
+            x = self.relu(x)
+            x,wd7 = self.fc3(x)
+            wd=wd1+wd2+wd3+wd4+wd5+wd6+wd7
+            return x, wd
+        else:
+            x=self.conv1(x)
+            x=self.relu(x)
+            x=self.conv2(x)
+            x=self.relu(x)
+            x=self.max_pool(x)
+            x=self.conv3(x)
+            x=self.relu(x)
+            x=self.conv4(x)
+            x=self.relu(x)
 
-        x=self.max_pool(x)
-        x = x.view(x.size(0), 8192)
-        x = self.fc1(x)
+            x=self.max_pool(x)
+            x = x.view(x.size(0), 8192)
+            x = self.fc1(x)
 
-        x = self.relu(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
-        return x, torch.tensor(0)
+            x = self.relu(x)
+            x = self.fc2(x)
+            x = self.relu(x)
+            x = self.fc3(x)
+            return x, torch.tensor(0)
 
 
 
@@ -162,8 +193,8 @@ def get_datasets(args):
         test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
         return train_loader, test_loader
     else:
-        dataset1 = datasets.CIFAR10(f'{args.base_dir}data', train=True, download=True, transform=transform)
-        dataset2 = datasets.CIFAR10(f'{args.base_dir}data', train=True, transform=transform)
+        dataset1 = datasets.CIFAR10(f'{args.base_dir}data', train=True, download=True, transform=train_transform)
+        dataset2 = datasets.CIFAR10(f'{args.base_dir}data', train=True, transform=test_transform)
         # split dataset in half by labels
         labels = torch.unique(dataset1.targets)
         ds1_labels = labels[:len(labels) // 2]
