7cd2fc64e6e557fa74916538637f0aa774934776
matthewgorbett <Idontknow8112@@@>
2022-12-21 12:12:17 -0700

    merge_single

--- cifar10_double.py
+++ cifar10_double.py
@@ -214,6 +214,8 @@ def get_datasets(args):
         dataset1.data, dataset1.targets = dataset1.data[:int(len(dataset1.targets)/2)], dataset1.targets[:int(len(dataset1.targets)/2)]
         dataset2.data, dataset2.targets = dataset2.data[int(len(dataset1.targets)/2):], dataset2.targets[int(len(dataset1.targets)/2):]
         '''
+        print(ds2_indices)
+        print(len(ds1_indices))
         dataset1.data, dataset1.targets = dataset1.data[ds1_indices], dataset1.targets[ds1_indices]
         dataset2.data, dataset2.targets = dataset2.data[ds2_indices], dataset2.targets[ds2_indices]
         assert (set(ds1_indices).isdisjoint(ds2_indices))
