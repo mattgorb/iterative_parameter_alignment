121ab3018a0dc0a9a8aefe6cb07a27011c1abcca
matthewgorbett <Idontknow8112@@@>
2022-12-21 20:14:50 -0700

    merge_single

--- mnist_double_detach.py
+++ mnist_double_detach.py
@@ -273,7 +273,7 @@ def main():
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--weight_seed', type=int, default=1, )
-    parser.add_argument('--gpu', type=int, default=1, )
+    parser.add_argument('--gpu', type=int, default=6, )
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     parser.add_argument('--baseline', type=bool, default=False, help='train base model')
