af1154f3a7fdf669b57ba3234f216aa6f277efe4
matthewgorbett <Idontknow8112@@@>
2022-12-30 22:22:49 -0700

    merge_single

--- mnist_double2.py
+++ mnist_double2.py
@@ -78,7 +78,7 @@ def get_datasets(args):
     #not using normalization
     transform=transforms.Compose([
         transforms.ToTensor(),
-        #transforms.Normalize((0.1307,), (0.3081,))
+        transforms.Normalize((0.1307,), (0.3081,))
         ])
 
     if args.baseline:
