38fa04e9be70c5898de5ec8cb3815f3d54e9ddb3
matthewgorbett <Idontknow8112@@@>
2023-01-20 20:46:57 -0700

    results

--- models/layers.py
+++ models/layers.py
@@ -38,6 +38,7 @@ class ConvMerge(nn.Conv2d):
         self.args = args
         _init_weight(args, self.weight)
         # self.args.weight_seed+=1
+        print(self.weight)
         print(self.bias)
         sys.exit()
 
