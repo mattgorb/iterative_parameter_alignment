2801289d373ec84b15b63496568b138a2e8369c3
matthewgorbett <Idontknow8112@@@>
2023-01-22 13:24:07 -0700

    dataset fix

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -115,6 +115,6 @@ def get_datasets(args):
                 f'\tTrain set {i}: Length: {len(train_loaders[i].dataset)}, Labels: {collections.Counter(train_loaders[i].dataset.targets.tolist())}')
 
         print(   f'\tTest set: Length: {len(test_loader.dataset)}, Labels: {collections.Counter(test_loader.dataset.targets)}')
-
+        sys.exit()
         return train_loaders, test_loader
 
