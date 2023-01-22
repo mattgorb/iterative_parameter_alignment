5ff1d0a765b7602372789eb8d99f00200fdecf6c
matthewgorbett <Idontknow8112@@@>
2023-01-22 14:03:45 -0700

    dataset fix

--- utils/trainer.py
+++ utils/trainer.py
@@ -48,6 +48,9 @@ class Trainer:
         for batch_idx, (data, target) in enumerate(self.train_loader):
 
             data, target = data.to(self.device), target.to(self.device)
+            print(data.size())
+
+            sys.exit()
             self.optimizer.zero_grad()
             output, weight_align = self.model(data)
             '''
