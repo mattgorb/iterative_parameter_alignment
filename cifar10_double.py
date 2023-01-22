75f02ee9069803cb7b453f823ce20a9c594822d9
matthewgorbett <Idontknow8112@@@>
2023-01-22 10:29:09 -0700

    results

--- cifar10_double.py
+++ cifar10_double.py
@@ -207,7 +207,7 @@ def get_datasets(args):
         return train_loader, test_loader
     else:
         dataset1 = datasets.CIFAR10(f'{args.base_dir}data', train=True, download=True, transform=train_transform)
-        #dataset2 = datasets.CIFAR10(f'{args.base_dir}data', train=True, transform=train_transform)
+        dataset2 = datasets.CIFAR10(f'{args.base_dir}data', train=True, transform=train_transform)
         # split dataset in half by labels
         labels = np.unique(dataset1.targets)
         ds1_labels = labels[:len(labels) // 2]
@@ -218,75 +218,25 @@ def get_datasets(args):
         ds1_indices = [idx for idx, target in enumerate(dataset1.targets) if target in ds1_labels]
         ds2_indices = [idx for idx, target in enumerate(dataset1.targets) if target in ds2_labels]
 
-
-        print('herre')
-        print(ds1_indices[:100])
-        print(ds2_indices[:100])
-        print('here')
-        print(ds1_labels)
-        print(ds2_labels)
-        print(len(ds1_indices))
-        print(len(ds2_indices))
-        assert (set(ds1_indices).isdisjoint(ds2_indices))
-        if args.imbalanced:
-            #use this code for p/1-p split.  need to test
-            p=0.75
-
-
-            print(int(len(ds1_indices)*p))
-            print(int(len(ds2_indices)*p))
-            print(len(ds1_indices[:int(len(ds1_indices)*p)]))
-            print(len(ds2_indices[int(len(ds2_indices)*p):]))
-            print(len(ds1_indices[int(len(ds1_indices)*p):]))
-            print(len(ds2_indices[:int(len(ds2_indices)*p)]))
-
-
-            x=ds1_indices[:int(len(ds1_indices)*p)]+ds2_indices[int(len(ds2_indices)*p):]
-            y=ds1_indices[int(len(ds1_indices)*p):]+ds2_indices[:int(len(ds2_indices)*p)]
-            print("HEREREEe")
-
-        x=set(x) & set(y)
-        print(x)
-
-        print(len(x))
-        assert (set(ds1_indices).isdisjoint(ds2_indices))
-        sys.exit()
-        print(len(ds1_indices))
-        print(len(ds2_indices))
-
-        x=list(np.array(dataset1.targets)[ds1_indices])
-        x2=list(np.array(dataset1.targets)[ds2_indices])
-
-
-        import collections
-        counter = collections.Counter(x)
-        counter2=collections.Counter(x2)
-        print(counter)
-        print(counter2)
-        sys.exit()
-
-
-        print(len(ds1_indices))
-        print(len(ds2_indices))
-
-        print(ds1_indices[:100])
-        print(ds2_indices[:100])
-
         '''
         #use this code to split dataset down middle. need to test
         dataset1.data, dataset1.targets = dataset1.data[:int(len(dataset1.targets)/2)], dataset1.targets[:int(len(dataset1.targets)/2)]
         dataset2.data, dataset2.targets = dataset2.data[int(len(dataset1.targets)/2):], dataset2.targets[int(len(dataset1.targets)/2):]
         '''
+        if args.imbalanced:
+            #use this code for p/1-p split.  need to test
+            p=0.75
 
-        dataset1.data,dataset1.targets = dataset1.data[ds1_indices],list(np.array(dataset1.targets)[ds1_indices])
-        dataset2.data, dataset1.targets = dataset2.data[ds2_indices], list(np.array(dataset2.targets)[ds2_indices])
-        #assert (set(ds1_indices).isdisjoint(ds2_indices))
+            d1=ds1_indices[:int(len(ds1_indices)*p)]+ds2_indices[int(len(ds2_indices)*p):]
+            d2=ds1_indices[int(len(ds1_indices)*p):]+ds2_indices[:int(len(ds2_indices)*p)]
+            dataset1.data,dataset1.targets = dataset1.data[d1],list(np.array(dataset1.targets)[d1])
+            dataset2.data, dataset1.targets = dataset2.data[d2], list(np.array(dataset2.targets)[d2])
+            assert (set(d1).isdisjoint(d2))
+        else:
+            dataset1.data,dataset1.targets = dataset1.data[ds1_indices],list(np.array(dataset1.targets)[ds1_indices])
+            dataset2.data, dataset1.targets = dataset2.data[ds2_indices], list(np.array(dataset2.targets)[ds2_indices])
+            assert (set(ds1_indices).isdisjoint(ds2_indices))
 
-
-
-        print(counter)
-        print(counter2)
-        sys.exit()
         test_dataset = datasets.CIFAR10(f'{args.base_dir}data', train=False, transform=test_transform)
         train_loader1 = DataLoader(dataset1, batch_size=args.batch_size, shuffle=True)
         train_loader2 = DataLoader(dataset2, batch_size=args.batch_size, shuffle=True)
