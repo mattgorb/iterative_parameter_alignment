6f28bbef5eb8e0f49bd4ee24a1760178a8b0b3ad
matthewgorbett <Idontknow8112@@@>
2023-01-22 13:20:34 -0700

    dataset fix

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -81,6 +81,7 @@ def get_datasets(args):
             else:
                 for group in index_groupings:
                     dataset = datasets.CIFAR10(f'{args.base_dir}data', train=True, transform=train_transform)
+                    print(group)
                     dataset.data, dataset.targets = dataset.data[group], dataset.targets[group]
                     train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
                     train_loaders.append(train_loader)
