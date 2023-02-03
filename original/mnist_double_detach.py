f754ccabbd10f0bffc1d38fbd0e43d4e21be7e91
matthewgorbett <Idontknow8112@@@>
2023-02-03 11:30:25 -0700

    mnist

--- original/mnist_double_detach.py
+++ original/mnist_double_detach.py
@@ -214,29 +214,18 @@ class Merge_Iterator:
         model2_trainer = Trainer(self.args, [self.train_loader2, self.test_dataset], model2, self.device,
                                  f'{self.weight_dir}model2_0.pt', 'model2_double')
 
-
-
-
         for iter in range(merge_iterations):
             if iter>0:
                 model1.fc1.weight_align=nn.Parameter(model2.fc1.weight.clone().detach().to(self.device), requires_grad=True)
                 model1.fc2.weight_align=nn.Parameter(model2.fc2.weight.clone().detach().to(self.device), requires_grad=True)
-            #if iter > 1:
-                #model1.fc1.weight = nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
-                #model1.fc2.weight = nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
-            model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
-
+                model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
             model1_trainer.fit()
 
             if iter>0:
                 model2.fc1.weight_align=nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
                 model2.fc2.weight_align=nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+                model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
-                #model2.fc1.weight=nn.Parameter(model1.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
-                #model2.fc2.weight=nn.Parameter(model1.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
-
-            model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
-            print(model2_trainer.model.fc2.weight[0][:10])
             model2_trainer.fit()
 
 
--- original/mnist_single_detach.py
+++ original/mnist_single_detach.py
@@ -157,21 +157,6 @@ class Trainer:
         self.model.train()
         train_loss = 0
 
-        if self.args.graphs:
-            if self.model.fc1.weight is not None:
-                self.fc1_norm_list.append(torch.norm(self.model.fc1.weight, p=1).detach().cpu().item())
-                self.fc2_norm_list.append(torch.norm(self.model.fc2.weight, p=1).detach().cpu().item())
-
-            if hasattr(self.model.fc1, 'weight_align'):
-                if self.model.fc1.weight_align is not None:
-                    self.wa1_norm_list.append(torch.norm(self.model.fc1.weight_align, p=1).detach().cpu().item())
-                    self.wa2_norm_list.append(torch.norm(self.model.fc2.weight_align, p=1).detach().cpu().item())
-
-                else:
-                    self.wa1_norm_list.append(None)
-                    self.wa2_norm_list.append(None)
-
-
         for batch_idx, (data, target) in enumerate(self.train_loader):
 
             data, target = data.to(self.device), target.to(self.device)
