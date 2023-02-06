94c1a3268cff4c0c37718740214be314225543bd
matthewgorbett <Idontknow8112@@@>
2023-02-05 20:36:57 -0700

    mnist

--- mnist_double.py
+++ mnist_double.py
@@ -68,9 +68,11 @@ class Net(nn.Module):
             x, wa2 = self.fc2(x)
             score_diff = wa1 + wa2
             if score_diff>0:
+
+                print('here')
                 print(wa1)
                 print(wa2)
-                sys.exit()
+                #sys.exit()
             return x, score_diff
         else:
             x = self.fc1(x.view(-1, 28 * 28))
