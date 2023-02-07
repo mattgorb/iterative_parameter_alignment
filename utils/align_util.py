583d50ca5ff44dc3950db7d4f5938692abfbb360
matthewgorbett <Idontknow8112@@@>
2023-02-06 19:06:25 -0700

    teset

--- utils/align_util.py
+++ utils/align_util.py
@@ -6,28 +6,20 @@ import torch.nn as nn
 def set_weight_align_param(models, args):
     module_list=[model.named_modules() for model in models]
     for named_layer_modules in zip(*module_list):
-        #n1, m1 = model1_mods[0]
-        #print(n1)
-
         if not type(named_layer_modules[0][1]) == LinearMerge and not type(named_layer_modules[0][1])==ConvMerge:
             continue
-        print(len(named_layer_modules))
-        print(named_layer_modules[0][1])
-        sys.exit()
+
         if hasattr(m1, "weight"):
-            '''
-            m1.weight gets updated to m2.weight_align because it is not detached.  and vice versa
-            This is a simple way to "share" the weights between models. 
-            Alternatively we could set m1.weight=m2.weight_align after merge model is done training.  
-            '''
-
-            print("Aligning...")
-            m2.weight_align_list.append(nn.Parameter(m1.weight, requires_grad=True))
-            m1.weight_align_list.append(nn.Parameter(m2.weight, requires_grad=True))
-
-            if args.bias:
-                m2.bias_align_list.append(nn.Parameter(m1.bias, requires_grad=True))
-                m1.bias_align_list.append(nn.Parameter(m2.bias, requires_grad=True))
+            for module_i in range(len(named_layer_modules)):
+                for module_j in range(len(named_layer_modules)):
+                    if module_j==module_i:
+                        continue
+                    else:
+                        print(f'Layer {named_layer_modules[module_i][0]}: Adding model {j} to {i} ')
+                        named_layer_modules[module_i][1].weight_align_list.append(nn.Parameter(named_layer_modules[module_j][1].weight, requires_grad=True))
+                        if args.bias:
+                            named_layer_modules[module_i][1].bias_align_list.append(nn.Parameter(named_layer_modules[module_j][1].bias, requires_grad=True))
+                            named_layer_modules[module_i][1].bias_align_list.append(nn.Parameter(named_layer_modules[module_j][1].bias, requires_grad=True))
 
 def set_weight_align_param_orig(models, args):
     for model1_mods, model2_mods, in zip(models[0].named_modules(), models[1].named_modules(),):
