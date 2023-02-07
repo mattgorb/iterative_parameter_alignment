a77f7d2b8cfd19630211169a6e39adbde3d99ff6
matthewgorbett <Idontknow8112@@@>
2023-02-06 19:07:31 -0700

    teset

--- utils/align_util.py
+++ utils/align_util.py
@@ -15,7 +15,7 @@ def set_weight_align_param(models, args):
                     if module_j==module_i:
                         continue
                     else:
-                        print(f'Layer {named_layer_modules[module_i][0]}: Adding model {j} to {i} ')
+                        print(f'Layer {named_layer_modules[module_i][0]}: Adding model {module_j} to {module_i} ')
                         named_layer_modules[module_i][1].weight_align_list.append(nn.Parameter(named_layer_modules[module_j][1].weight, requires_grad=True))
                         if args.bias:
                             named_layer_modules[module_i][1].bias_align_list.append(nn.Parameter(named_layer_modules[module_j][1].bias, requires_grad=True))
