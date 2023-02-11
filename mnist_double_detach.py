261e2b66383d5657e839144519ec04335b7c4066
matthewgorbett <Idontknow8112@@@>
2023-02-10 20:41:58 -0700

    mnist detach

--- mnist_double_detach.py
+++ mnist_double_detach.py
@@ -42,9 +42,9 @@ class LinearMerge(nn.Linear):
         weights_diff = torch.tensor(0)
         if self.weight_align is not None:
             #print(self.weight-self.weight_align)
-            #weights_diff = torch.sum((self.weight - self.weight_align).abs())*self.weight.size(1)
+            weights_diff = torch.sum((self.weight - self.weight_align).abs())*self.weight.size(1)
 
-            weights_diff = torch.sum(torch.square(self.weight - self.weight_align))*self.weight.size(1)
+            #weights_diff = torch.sum(torch.square(self.weight - self.weight_align))*self.weight.size(1)
             #print(weights_diff)
             #sys.exit()
         return x, weights_diff
