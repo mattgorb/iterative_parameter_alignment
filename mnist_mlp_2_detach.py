d8a248460b1bd25bd9e2438c4c4d51530c3c4b25
matthewgorbett <Idontknow8112@@@>
2023-02-12 14:48:51 -0700

    detach

--- mnist_mlp_2_detach.py
+++ mnist_mlp_2_detach.py
@@ -240,7 +240,7 @@ class Merge_Iterator:
                 model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
                 model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
             print(self.model1_trainer.optimizer.state_dict())
-            self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
+            #self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
 
             self.model1_trainer.fit()
 
@@ -250,7 +250,7 @@ class Merge_Iterator:
             if self.args.set_weight_from_weight_align:
                 model2.fc1.weight=nn.Parameter(model1.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
                 model2.fc2.weight=nn.Parameter(model1.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
-            self.model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
+            #self.model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
             self.model2_trainer.fit()
 
