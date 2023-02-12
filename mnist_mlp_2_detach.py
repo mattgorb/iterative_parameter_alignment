6445e820e2c4fb2baabb02304006445c7fbc5db4
matthewgorbett <Idontknow8112@@@>
2023-02-12 14:50:37 -0700

    detach

--- mnist_mlp_2_detach.py
+++ mnist_mlp_2_detach.py
@@ -229,6 +229,7 @@ class Merge_Iterator:
         model1.fc2.weight_align = nn.Parameter(model2.fc2.weight.clone().detach().to(self.device), requires_grad=True)
         model2.fc1.weight_align = nn.Parameter(model1.fc1.weight.clone().detach().to(self.device), requires_grad=True)
         model2.fc2.weight_align = nn.Parameter(model1.fc2.weight.clone().detach().to(self.device), requires_grad=True)
+
         self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
         self.model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
@@ -242,7 +243,7 @@ class Merge_Iterator:
                 model1.fc1.weight=nn.Parameter(model2.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
                 model1.fc2.weight=nn.Parameter(model2.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
             print(self.model1_trainer.optimizer.state_dict())
-            #self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
+            self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
 
             self.model1_trainer.fit()
 
@@ -252,7 +253,7 @@ class Merge_Iterator:
             if self.args.set_weight_from_weight_align:
                 model2.fc1.weight=nn.Parameter(model1.fc1.weight_align.clone().detach().to(self.device), requires_grad=True)
                 model2.fc2.weight=nn.Parameter(model1.fc2.weight_align.clone().detach().to(self.device), requires_grad=True)
-            #self.model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
+            self.model2_trainer.optimizer = optim.Adam(model2.parameters(), lr=self.args.lr)
 
             self.model2_trainer.fit()
 
