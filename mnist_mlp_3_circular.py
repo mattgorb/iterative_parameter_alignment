861676c221762c1a11345d605ba509b17c4aa923
matthewgorbett <Idontknow8112@@@>
2023-02-18 08:56:08 -0700

    dirichllte

--- mnist_mlp_3_circular.py
+++ mnist_mlp_3_circular.py
@@ -298,7 +298,7 @@ class Merge_Iterator:
             transfer_state_dict=self.model1_trainer.optimizer.state_dict()
             self.model1_trainer.optimizer = optim.Adam(model1.parameters(), lr=self.args.lr)
             self.model1_trainer.optimizer.load_state_dict(transfer_state_dict)
-            print(f'summary: {print(torch.cuda.memory_summary())}')
+            print(f'summary: {print(torch.cuda.memory_summary(7))}')
             print(f"1: {torch.cuda.memory_allocated(self.args.gpu)}")
             self.model1_trainer.fit()
             print(f"2: {torch.cuda.memory_allocated(self.args.gpu)}")
@@ -325,7 +325,7 @@ class Merge_Iterator:
 
             print(f"3: {torch.cuda.memory_allocated(self.args.gpu)}")
 
-            print(f'summary: {print(torch.cuda.memory_summary())}')
+            print(f'summary: {print(torch.cuda.memory_summary(7))}')
             sys.exit()
             self.model2_trainer.fit()
             print(f"4: {torch.cuda.memory_allocated(self.args.gpu)}")
