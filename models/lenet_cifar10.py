704a52e3bb9f8350d97d5384083bacbffd46bd08
matthewgorbett <Idontknow8112@@@>
2023-02-19 08:32:57 -0700

    dirichllte

--- models/lenet_cifar10.py
+++ models/lenet_cifar10.py
@@ -36,7 +36,7 @@ class LeNetCifar10(nn.Module):
             self.conv2 = nn.Conv2d(6, 16, 5)
             self.fc1 = nn.Linear(16 * 5 * 5, 120)
             self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, args.num_classes)
+            self.fc3 = nn.Linear(84, 10)
 
 
         self.max_pool=nn.MaxPool2d((2, 2))
