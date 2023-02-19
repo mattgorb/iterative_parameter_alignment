86d0f12f03277e7c8822c4e97dad5a883c6a41ad
matthewgorbett <Idontknow8112@@@>
2023-02-19 08:32:15 -0700

    dirichllte

--- models/lenet_cifar10.py
+++ models/lenet_cifar10.py
@@ -25,13 +25,18 @@ class LeNetCifar10(nn.Module):
             self.fc3=linear_init(192, 10, args=self.args)
         else:
             self.n_cls = 10
-            self.conv1 = nn.Conv2d(in_channels=3, out_channels=64 , kernel_size=5,   bias=self.bias)
+            '''self.conv1 = nn.Conv2d(in_channels=3, out_channels=64 , kernel_size=5,   bias=self.bias)
             self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5,  bias=self.bias)
             self.pool = nn.MaxPool2d(kernel_size=2, stride=2,)
             self.fc1 = nn.Linear(64*5*5, 384, bias=self.bias)
             self.fc2 = nn.Linear(384, 192, bias=self.bias)
-            self.fc3 = nn.Linear(192, 10, bias=self.bias)
-
+            self.fc3 = nn.Linear(192, 10, bias=self.bias)'''
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, args.num_classes)
 
 
         self.max_pool=nn.MaxPool2d((2, 2))
@@ -61,7 +66,8 @@ class LeNetCifar10(nn.Module):
         else:
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 64*5*5)
+            #x = x.view(-1, 64*5*5)
+            x = x.view(-1, 16*5*5)
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
