a0470f88d07089fcd7742018f9facce87db6b87a
matthewgorbett <Idontknow8112@@@>
2023-02-20 19:30:10 -0700

    dirichllte

--- mnist_examples/mnist_mlp_2.py
+++ mnist_examples/mnist_mlp_2.py
@@ -47,7 +47,7 @@ class LinearMerge(nn.Linear):
         x = F.linear(x, self.weight, self.bias)
         weights_diff_ae = torch.tensor(0)
         weights_diff_se = torch.tensor(0)
-        print(self.alpha)
+        print(self.delta)
         if self.weight_align is not None:
             #weights_diff_ae = torch.sum((self.weight - self.weight_align).abs())
             weights_diff_se = torch.sum(torch.square(self.weight - self.weight_align))
