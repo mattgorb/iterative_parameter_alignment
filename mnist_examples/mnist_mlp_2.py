977f20db610560ab05e4f9d9f3897dd5924e336e
matthewgorbett <Idontknow8112@@@>
2023-02-21 13:05:44 -0700

    dirichllte

--- mnist_examples/mnist_mlp_2.py
+++ mnist_examples/mnist_mlp_2.py
@@ -44,7 +44,7 @@ class LinearMerge(nn.Linear):
         #nn.init.kaiming_normal_(self.weight, mode="fan_in", nonlinearity="relu")
 
     def forward(self, x):
-        x = F.linear(x, self.weight, self.bias)
+        out = F.linear(x, self.weight, self.bias)
 
         weights_diff_ae = torch.tensor(0)
         weights_diff_se = torch.tensor(0)
@@ -57,12 +57,12 @@ class LinearMerge(nn.Linear):
                                         #self.delta*((self.weight - self.weight_align)-0.5*self.delta)
                                         #))
             #weights_diff_ae=self.loss(self.weight,self.weight_align)
-        print(self.weight.size())
-        print(self.weight_align.size())
-        print(F.linear(x, self.weight, self.bias).size())
-        print(F.linear(x, self.weight_align, self.bias).size())
-        weights_diff_ae=(F.linear(x, self.weight, self.bias)-F.linear(x, self.weight_align, self.bias) )#**self.delta
-        return x, weights_diff_ae, weights_diff_se
+        #print(self.weight.size())
+        #print(self.weight_align.size())
+        #print(F.linear(x, self.weight, self.bias).size())
+        #print(F.linear(x, self.weight_align, self.bias).size())
+        weights_diff_ae=torch.sum((F.linear(x, self.weight, self.bias)-F.linear(x, self.weight_align, self.bias) )**self.delta)
+        return out, weights_diff_ae, weights_diff_se
 
 class Net(nn.Module):
     def __init__(self, args, weight_merge=False):
