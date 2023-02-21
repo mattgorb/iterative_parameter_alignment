339b4f31eab19c570d1181167e0aaf3462bacda7
matthewgorbett <Idontknow8112@@@>
2023-02-21 13:01:42 -0700

    dirichllte

--- mnist_examples/mnist_mlp_2.py
+++ mnist_examples/mnist_mlp_2.py
@@ -45,6 +45,7 @@ class LinearMerge(nn.Linear):
 
     def forward(self, x):
         x = F.linear(x, self.weight, self.bias)
+
         weights_diff_ae = torch.tensor(0)
         weights_diff_se = torch.tensor(0)
         print(self.delta)
@@ -56,6 +57,7 @@ class LinearMerge(nn.Linear):
                                         #self.delta*((self.weight - self.weight_align)-0.5*self.delta)
                                         #))
             #weights_diff_ae=self.loss(self.weight,self.weight_align)
+        weights_diff_ae=(F.linear(x, self.weight, self.bias)-F.linear(x, self.weight_align, self.bias) )**self.delta
         return x, weights_diff_ae, weights_diff_se
 
 class Net(nn.Module):
