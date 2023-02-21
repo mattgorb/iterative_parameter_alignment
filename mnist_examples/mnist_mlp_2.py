aba84925b2c85a5a1101cfe29e24f1ec260e9879
matthewgorbett <Idontknow8112@@@>
2023-02-20 18:39:46 -0700

    dirichllte

--- mnist_examples/mnist_mlp_2.py
+++ mnist_examples/mnist_mlp_2.py
@@ -305,7 +305,7 @@ def main():
                         help='number of epochs to train')
     parser.add_argument('--merge_iter', type=int, default=2500,
                         help='number of iterations to merge')
-    parser.add_argument('--weight_align_factor', type=int, default=250, )
+    parser.add_argument('--weight_align_factor', type=int, default=25, )
     parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7,
