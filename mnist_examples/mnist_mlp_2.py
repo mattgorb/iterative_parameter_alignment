58ec072dee57b79bc7500cadbe46a5d5f0a8ee21
matthewgorbett <Idontknow8112@@@>
2023-02-21 19:44:12 -0700

    mnist

--- mnist_examples/mnist_mlp_2.py
+++ mnist_examples/mnist_mlp_2.py
@@ -37,7 +37,7 @@ class LinearMerge(nn.Linear):
         self.weight_align = None
         #self.loss=torch.nn.HuberLoss(reduction='sum')
         self.delta=nn.Parameter(torch.ones(1), requires_grad=True)
-        self.alpha=1.005
+        self.alpha=1.002
         self.n=1
 
     def init(self, args):
