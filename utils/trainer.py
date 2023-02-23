e85155d91fd94042a1e246dc7d24e50ae8de4058
matthewgorbett <Idontknow8112@@@>
2023-02-22 22:14:40 -0700

    mnist

--- utils/trainer.py
+++ utils/trainer.py
@@ -11,6 +11,7 @@ class Trainer:
         self.args = args
         self.model = model
         self.train_loader, self.test_loader = datasets[0], datasets[1]
+
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
 
 
