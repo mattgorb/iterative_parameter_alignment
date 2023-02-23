38ea77fbda415b991506fcd1de101b4757910939
matthewgorbett <Idontknow8112@@@>
2023-02-23 09:25:41 -0700

    mnist

--- utils/trainer.py
+++ utils/trainer.py
@@ -41,9 +41,9 @@ class Trainer:
             #checkpoint = torch.load(self.save_path)
             #self.optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
             #self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            self.optimizer = optim.SGD(self.model.parameters(), lr=0.1*(0.998**self.train_iter), weight_decay=1e-3)
-            self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=1)
-
+            #self.optimizer = optim.SGD(self.model.parameters(), lr=0.1*(0.998**self.train_iter), weight_decay=1e-3)
+            #self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=1)
+            sys.exit()
         for epoch in range(1, self.args.local_epochs + 1):
             self.train()
 
