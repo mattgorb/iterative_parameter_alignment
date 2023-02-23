c3bcab1d508cd49212d5e02dafba5c8445265881
matthewgorbett <Idontknow8112@@@>
2023-02-23 09:22:24 -0700

    mnist

--- utils/trainer.py
+++ utils/trainer.py
@@ -15,8 +15,8 @@ class Trainer:
         #self.optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr)
 
 
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.1,  weight_decay=1e-5)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=1)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.1,  weight_decay=1e-3)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=.992)
 
 
         self.criterion = nn.CrossEntropyLoss(reduction='sum')
@@ -51,7 +51,7 @@ class Trainer:
             self.test_loss = test_loss
             self.test_acc = test_acc
 
-            #self.scheduler.step()
+            self.scheduler.step()
 
             if log_output:
                 print( f'Local Epoch: {epoch}, Train loss: {self.train_loss}, Test loss: {self.test_loss}, Test Acc: {self.test_acc}')
