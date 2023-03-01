fda5ff925424721b282dd78c91af4800d674526c
matthewgorbett <Idontknow8112@@@>
2023-03-01 13:35:45 -0700

    federated learning

--- mnist_examples/mnist_test.py
+++ mnist_examples/mnist_test.py
@@ -189,6 +189,7 @@ def main():
 
     print(model1.fc1.weight[0][:10])
     print(model2.fc1.weight[0][:10])
+    sys.exit()
 
     model_merge = Net(args, ).to(device)
     optim_merge = optim.Adam(model_merge.parameters(), lr=args.lr)
