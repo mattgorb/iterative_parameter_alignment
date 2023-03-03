5e329852aad1430195e523194809255a9f8e79ed
matthewgorbett <Idontknow8112@@@>
2023-03-02 21:54:22 -0700

    federated learning

--- federated_learning/utils_dataset.py
+++ federated_learning/utils_dataset.py
@@ -229,7 +229,7 @@ class DatasetObject:
                 print(f'ds1_labels: {ds1_labels}')
                 print(f'ds2_labels: {ds2_labels}')
                 ds1_indices = [idx for idx, target in enumerate(trn_y) if target in ds1_labels]
-                ds2_indices = [idx for idx, target in enumerate(trn_y) if target in ds1_labels]
+                ds2_indices = [idx for idx, target in enumerate(trn_y) if target in ds2_labels]
 
 
                 clnt_x[0]=trn_x[ds1_indices]
