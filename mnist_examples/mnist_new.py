981e6476519648cd106949210d4c5c626cff15e7
matthewgorbett <Idontknow8112@@@>
2023-03-03 18:15:28 -0700

    federated learning

--- mnist_examples/mnist_new.py
+++ mnist_examples/mnist_new.py
@@ -293,10 +293,10 @@ class Merge_Iterator:
                   f'\tModel 2 Train loss: {self.model2_trainer.train_loss},Train align loss: {self.model1_trainer.train_align_loss},Test loss: {self.model2_trainer.test_loss},  Test accuracy: {self.model2_trainer.test_acc}')
 
             print('distances')
-            print(torch.sum((model1.fc1.weight-model2.fc1.weight).abs()))
-            print(torch.sum((model1.fc2.weight-model2.fc2.weight).abs()))
-            print(torch.sum((model1.fc1.weight-model2.fc1.weight)**2))
-            print(torch.sum((model1.fc2.weight-model2.fc2.weight)**2))
+            print(torch.sum((model1.fc1.weight-model2.fc1.weight).abs()).item())
+            print(torch.sum((model1.fc2.weight-model2.fc2.weight).abs()).item())
+            print(torch.sum((model1.fc1.weight-model2.fc1.weight)**2).item())
+            print(torch.sum((model1.fc2.weight-model2.fc2.weight)**2).item())
 
 
 def main():
