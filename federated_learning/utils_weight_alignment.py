58a6dd6479b80b230a59dc0798a0a4b405359fe2
matthewgorbett <Idontknow8112@@@>
2023-03-05 22:58:29 -0700

    federated learning

--- federated_learning/utils_weight_alignment.py
+++ federated_learning/utils_weight_alignment.py
@@ -423,7 +423,7 @@ def train_weight_alignment(data_obj, act_prob, learning_rate, batch_size, epoch,
             opt=optim.Adam(global_model.parameters(), lr=1e-3)
             for i in range(10):
                 model.train()
-                for j in range(1):
+                for j in range(25):
                     opt.zero_grad()
 
                     _,align_out = global_model(torch.randn(50,28,28).to(device))
