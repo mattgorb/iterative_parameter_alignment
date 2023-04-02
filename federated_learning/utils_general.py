2800b204a916fe279c10bd98bf1ce0f9a6dc2e5c
Matthew Gorbett <matthewgorbett@Matthews-MacBook-Air.local>
2023-04-01 21:34:24 -0600

    a

--- federated_learning/utils_general.py
+++ federated_learning/utils_general.py
@@ -3,7 +3,7 @@ from utils_dataset import *
 from utils_models import *
 # Global parameters
 os.environ["CUDA_DEVICE_ORDER"]    = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 from torch.utils.tensorboard import SummaryWriter
 
