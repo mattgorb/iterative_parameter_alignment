d7f618dd3d4acd1ba46d0fac77f52383341f72fb
matthewgorbett <Idontknow8112@@@>
2023-04-14 11:08:29 -0600

    fashion mnist

--- federated_learning/example_code_fashionmnist.py
+++ federated_learning/example_code_fashionmnist.py
@@ -60,7 +60,7 @@ n_client = 20
 optim='SGD'
 '''
 
-
+'''
 rule='Drichlet'
 rule_arg=0.3
 n_client = 20
@@ -72,7 +72,7 @@ rule='split_label5'
 rule_arg=0.3
 n_client = 5
 optim='SGD'
-'''
+
 
 
 # IF Adam, import utils_general_adam in utils_methods file
