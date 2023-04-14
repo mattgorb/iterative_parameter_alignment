63f978bf3ffd9f6268e216f4ac87b98387c64c62
matthewgorbett <Idontknow8112@@@>
2023-04-13 22:38:06 -0600

    fashion mnist

--- federated_learning/example_code_cifar10.py
+++ federated_learning/example_code_cifar10.py
@@ -27,12 +27,12 @@ data_path = '/s/luffy/b/nobackup/mgorb/'  # The folder to save Data & Model
 
 #rule_arg=0.6
 
-#n_client = 2
-#rule='split_label'
+n_client = 2
+rule='split_label'
 
-rule='Drichlet'
+#rule='Drichlet'
 #rule='iid'
-rule_arg=0.6
+#rule_arg=0.6
 n_client = 20
 optim='SGD'
 
@@ -89,7 +89,7 @@ else:
 
 ####
 
-print('FedDC')
+'''print('FedDC')
 
 epoch = 5
 alpha_coef = 1e-2
@@ -105,7 +105,8 @@ n_minibatch = (epoch*n_iter_per_epoch).astype(np.int64)
                                     com_amount=com_amount, print_per=print_per, weight_decay=weight_decay, 
                                     model_func=model_func, init_model=init_model, alpha_coef=alpha_coef,
                                     sch_step=1, sch_gamma=1,save_period=save_period, suffix=suffix, trial=True,
-                                    data_path=data_path, lr_decay_per_round=lr_decay_per_round)
+                                    data_path=data_path, lr_decay_per_round=lr_decay_per_round)'''
+
 #exit(0)
 ###
 # baselines
