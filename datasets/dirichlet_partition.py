f64a7e33ea2ff4eb970c1f93d9ec3a03427a46ca
matthewgorbett <Idontknow8112@@@>
2023-04-15 17:21:52 -0600

    fashion mnist

--- datasets/dirichlet_partition.py
+++ datasets/dirichlet_partition.py
@@ -21,7 +21,8 @@ def record_net_data_stats(y_train, net_dataidx_map, args):
         net_cls_counts[net_i] = tmp
 
     print(f'Data statistics: {net_cls_counts}')
-
+    pritn(net_cls_counts)
+    sys.exit()
 
 
     with open(f'{args.base_dir}weight_alignment_csvs/{save_str}.pkl', 'wb') as f:
