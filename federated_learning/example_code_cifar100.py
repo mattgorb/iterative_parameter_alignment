b919b76a21ba8d97799e3881dcf1dbefa9def80b
matthewgorbett <Idontknow8112@@@>
2023-04-17 14:59:42 -0600

    fashion mnist

--- federated_learning/example_code_cifar100.py
+++ federated_learning/example_code_cifar100.py
@@ -28,7 +28,7 @@ data_path = '/s/luffy/b/nobackup/mgorb/'  # The folder to save Data & Model
 
 rule='iid'
 rule_arg=0.3
-n_client = 20
+n_client = 10
 optim='SGD'
 
 data_obj = DatasetObject(dataset='CIFAR100', n_client=n_client, seed=23, rule=rule, rule_arg=rule_arg,unbalanced_sgm=0, data_path=data_path)
