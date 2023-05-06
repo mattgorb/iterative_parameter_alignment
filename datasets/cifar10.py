15f9ca45b7c2297bb612d34a1440a51a3de7d4fc
matt <matt.gorbett@yahoo.com>
2023-05-05 18:31:38 -0600

    a

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -118,7 +118,7 @@ def get_datasets(args):
         elif args.dataset_split == 'classimbalance':
             data_prepper = Data_Prepper(
                 'cifar10', train_batch_size=args.batch_size, n_agents=num_clients,
-                sample_size_cap=600,
+                sample_size_cap=6000,
                 train_val_split_ratio=0.8, device=args.device,
                 args_dict=args)
 
